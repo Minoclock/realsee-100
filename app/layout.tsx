0e65986542a3bd381aa5d132461aa6006d7f0fab
Minoclock <lijinzhongljz@outlook.com>
2025-07-10 01:43:56 +0800

    Add files via upload

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 export default function RootLayout({ children }) {
   return (
     <html lang="zh">
-      <body style={{ fontFamily: 'sans-serif', padding: 40, backgroundColor: '#f8f8f8' }}>
+      <body style={{ fontFamily: 'sans-serif', padding: 40, backgroundColor: '#f9f9f9' }}>
         {children}
       </body>
     </html>
